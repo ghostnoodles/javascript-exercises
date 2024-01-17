2272d9b55fdd49759cf7c97471ec1c7aea0a78e1
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-17 21:21:24 +1300

    Learnt my lesson to not merge with main branch

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr) {
+    let argumentArray = []
+    for (let i = 1; i < arguments.length; i++){
+        argumentArray.push(arguments[i])
+        };
+    return arr.filter((value) => !argumentArray.includes(value))    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function(sumOne, sumTwo) {
+    let sumResult = 0 
+    if (sumOne < sumTwo){
+        for (let i = sumOne; i <= sumTwo; i++){
+            sumResult += i
+        }
+    }
+    else {
+        for (let i = sumTwo; i <= sumOne; i++){
+            sumResult += i
+        }
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
