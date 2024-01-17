eac156ab19bde1c644df42195b597cfe3643eda9
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-17 21:37:09 +1300

    Rewritten answer to attempt to pass Q.4,5, and 6 of 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,20 @@
 const sumAll = function(sumOne, sumTwo) {
     let sumResult = 0 
-    if (sumOne < sumTwo){
+
+    if ((sumOne < sumTwo) && (sumOne.typeof() == Number && sumTwo.typeof() == Number) && (sumOne > 0 && sumTwo > 0)){
         for (let i = sumOne; i <= sumTwo; i++){
             sumResult += i
         }
     }
-    else {
+    else if ((sumTwo < sumOne) && (sumOne.typeof() == Number && sumTwo.typeof() == Number) && (sumOne > 0 && sumTwo > 0)) {
         for (let i = sumTwo; i <= sumOne; i++){
             sumResult += i
         }
     }
+    else return 'ERROR'
+
+    return sumResult
+};
 
 // Do not edit below this line
 module.exports = sumAll;
