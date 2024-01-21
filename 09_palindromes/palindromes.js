8b874eafbd82b13ca7d76f0f7750256a2112e608
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-21 20:12:21 +1300

    Rewritten funtion code for palindromes to solve all test for Q 09_palindromes and comment old code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 function palindromes (palindromeWord) {
-    palindromeWord = palindromeWord.toLowerCase()
-    if (!/^[a-zA-Z()]+$/.test(palindromeWord)) palindromeWord = palindromeWord.slice(0, -1);
+    palindromeWord = palindromeWord.replace(/[^a-zA-Z0-9]/g, '').trim().toLowerCase()
+    // if (!/^[a-zA-Z()]+$/.test(palindromeWord)) palindromeWord = palindromeWord.filter((value) => );
     let palindromeReverse = palindromeWord.split('').reverse().join('');
     return palindromeWord == palindromeReverse ? true:false;
 };
 
+// palindromeWord = palindromeWord.slice(0, -1)
+// arr.filter((value) => !argumentArray.includes(value))  
 // Do not edit below this line
 module.exports = palindromes;
